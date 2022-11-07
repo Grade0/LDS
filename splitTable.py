8bc6d5715d7bdec1efe32100aba47ae72b8af66c
Davide Chen <60269150+Grade0@users.noreply.github.com>
2022-11-07 22:01:59 +0100

    update assignment 1
    - split table function
    - iscorrect column

--- splitTable.py
+++ splitTable.py
@@ -11,20 +11,55 @@ import csv
 
 answer_full = Path("Dataset/answerdatacorrect.csv")
 
+
 # Define the answer table
 answerTable = Path("Tables/answer.csv")
-answerHeader = ["AnswerId", "QuestionId", "UserId", "OrganizationId", "DateId", "SubjectId"]
-answerOthers = ["answer_value", "correct_answer", "iscorrect", "confidence"]
+answerHeader = ["QuestionId", "UserId", "AnswerId", "SubjectId", "OrganizationId", "DateId"]
+answerOthers = ["AnswerValue", "CorrectAnswer", "Confidence", "iscorrect"]
 
-print(f"Extracting {answerTable}\nwith header: {answerHeader}…")
-ids = set()
-with open(answerTable, mode="w") as targetFile:
-    targetFile.write(f"{','.join(answerHeader)}\n")
-    with open(answer_full) as sourceFile:
-        sourceRows = csv.DictReader(sourceFile)
-        for row in sourceRows:
-            to_write = ""
-            for row_key, row_value in row.items():
-                if row_key in answerHeader:
-                    to_write = f"{to_write},{row_value}"
-            targetFile.write(f"{to_write[1:]}\n")
+organizationTable = Path("Tables/organization.csv")
+organizationHeader = ["Organizationid"]
+organizationOthers = ["GroupId", "QuizId", "SchemeOfWorkId"]
+
+dateTable = Path("Tables/date.csv")
+dateHeader = ["DateId"]
+dateOthers = ["Date", "Day", "Month", "Year", "Quarter"]
+
+subjectTable = Path("Tables/subject.csv")
+subjectHeader = ["SubjectId"]
+subjectOthers = ["Description"]
+
+userTable = Path("Tables/user.csv")
+userHeader = ["UserId", "DateOfBirthId", "GeoId"]
+userOthers = ["Gender"]
+
+geoTable = Path("Tables/geography.csv")
+geoHeader = ["GeoId"]
+geoOthers = ["Region", "Country_Name", "Continent"]
+
+def extract_table(file, header):
+    print(f"Extracting {file}\nwith header: {header}…")
+    ids = -1
+    with open(file, mode="w+") as targetFile:
+        targetFile.write(f"{','.join(header)}\n")
+        with open(answer_full) as sourceFile:
+            sourceRows = csv.DictReader(sourceFile)
+            for row in sourceRows:
+                to_write = ""
+                for row_key, row_value in row.items():
+                    if row_key in header:
+                        to_write = f"{to_write},{row_value}"
+                if "answer" in file.stem:
+                    if row["AnswerValue"] == row["CorrectAnswer"]:
+                        to_write = f"{to_write},1"
+                    else:
+                        to_write = f"{to_write},0"
+                targetFile.write(f"{to_write[1:]}\n")
+
+                
+extract_table(answerTable , answerOthers)   
+            
+            
+# problemi da risolvere:
+    #manca OrganizationId, DateId, GeoId (come generarle? In che ordine?)
+    #SubjectId prensenta delle virgole la suo interno (conflitto con delimitatore)
